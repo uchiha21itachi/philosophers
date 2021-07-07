9a726980f5409da13deefb703e7dd6db20d1689e
Yash SHARMA <yash221b@gmail.com>
2021-07-07 06:43:22 +0200

    addede time func

--- philosophers.c
+++ philosophers.c
@@ -26,9 +26,26 @@ t_cond  *cond_init(int argc, char **argv)
     return (data);
 }
 
+unsigned long get_time(void)
+{
+    struct timeval  tp;
+    unsigned long   temp;
+
+    gettimeofday(&tp, NULL);
+    temp = tp.tv_sec;
+    // printf("temp [%lu]\n", temp);
+    temp = tp.tv_usec;
+    // printf("temp [%lu]\n", temp);
+    temp = (tp.tv_sec * 1000) + (tp.tv_usec / 1000);
+    printf("temp [%lu]\n", temp);
+    return (temp);
+}
+
 void    philo_init(t_cond *data)
 {
-    int     i;
+    int             i;
+    unsigned int    cur_time;
+    unsigned int    now_time;
 
     i = 0;
     data->philo = (t_philo *)malloc(sizeof(t_philo) * data->total_philo);
@@ -37,6 +54,11 @@ void    philo_init(t_cond *data)
         printf("Error in Malloc in philo_init\n");
         return ;
     }
+    cur_time = get_time();
+    usleep(2000000
+    );
+    now_time = get_time();
+    printf("time diff [%u]\n", (now_time - cur_time));
     while (i < data->total_philo)
     {
         data->philo[i].id = i;
@@ -52,17 +74,19 @@ int main(int argc, char **argv)
     i = 0;
     data = cond_init(argc, argv);
     philo_init(data);
-    printf("Total_philo [%d]\n", data->total_philo);
-    printf("Time to die [%d]\n", data->time_die);
-    printf("Time to eat [%d]\n", data->time_eat);
-    printf("Time to sleep[%d]\n", data->time_sleep);
-    printf("Total number of required meals[%d]\n",
-    data->total_time_eat);
-    while (i < data->total_philo)
-    {
-        printf("philosopher number [%d]\n", data->philo[i].id);
-        i++;
-    }
+    
+    // printf("Total_philo [%d]\n", data->total_philo);
+    // printf("Time to die [%d]\n", data->time_die);
+    // printf("Time to eat [%d]\n", data->time_eat);
+    // printf("Time to sleep[%d]\n", data->time_sleep);
+    // printf("Total number of required meals[%d]\n",
+    // data->total_time_eat);
+    // while (i < data->total_philo)
+    // {
+    //     printf("philosopher number [%d]\n", data->philo[i].id);
+    //     i++;
+    // }
+    free(data->philo);
     free(data);
    return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
 
 typedef struct s_philo
 {
